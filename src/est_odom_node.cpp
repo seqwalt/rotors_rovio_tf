cdd1ebee9316754a6387d4fd7a16a0a8bf1f1688
sequoyah <sequoyah.n.walters@gmail.com>
2023-01-02 17:03:46 -0600

    fix rovio initialization issue

--- src/est_odom_node.cpp
+++ src/est_odom_node.cpp
@@ -16,6 +16,13 @@ nav_msgs::Odometry new_msg;
 bool est_odom_unavail = true;
 bool first_gt_callback = true;
 bool first_rovio_callback = true;
+float rovio_start_time;
+bool do_once = true;
+double tot_secs = 60.0; // take one minute to initialize Rovio
+double sec_counter = tot_secs;
+tf2::Vector3 init_rovio_pos;
+tf2::Quaternion init_rWI_q;
+tf2::Vector3 init_angVel, init_linVel;
 
 void ground_truth_callback(const nav_msgs::Odometry &msg){
   last_gt_msg = msg; // will be used to initialize est_odom_callback
@@ -32,8 +39,21 @@ void est_odom_callback(const nav_msgs::Odometry &msg){
   new_msg = msg;
   geometry_msgs::Quaternion msg_q = msg.pose.pose.orientation; // quaternion part of msg
   tf2::Quaternion IB_q = tf2::Quaternion(0.0, 0.38268, 0.0, -0.92388); // tf quaternion [x,y,z,w] for rotation from imu to quad body (45 deg about y-axis)
-
   if (first_rovio_callback){
+    rovio_start_time = msg.header.stamp.toSec();
+    first_rovio_callback = false;
+    ROS_INFO("ROVIO initialization done in:");
+  }
+
+  double time_diff = msg.header.stamp.toSec() - rovio_start_time;
+  bool initializing_rovio = time_diff < tot_secs; // rovio is initialized for the first tot_secs seconds
+  if (initializing_rovio){
+    // enter this if-statement multiple times until rovio is sufficiently initialized
+    if (tot_secs - time_diff <= sec_counter){
+      ROS_INFO("%.1f seconds",tot_secs - time_diff);
+      sec_counter -= 1.0;
+    }
+  } else {
     // ROTATE rovio orientation to align with gt orientation (since rovio orientation aligns with down-tilted imu)
     // ----------------------------------------------------------------
     geometry_msgs::Quaternion msg_gt_q = last_gt_msg.pose.pose.orientation; // last ground truth quaternion
@@ -43,70 +63,45 @@ void est_odom_callback(const nav_msgs::Odometry &msg){
     tf2::Quaternion rWI_q; // quaternion for rotation from rovio world (rW) x-axis to imu (I)
     tf2::fromMsg(msg_q, rWI_q); // load rWI_q with data
 
-    tf2::Quaternion sWI_q = q_mult(sWrW_q, rWI_q); // quaternion for rotation from simulation world to imu
-    new_msg.pose.pose.orientation = tf2::toMsg(sWI_q); // convert to message format
+    if (do_once){
+      tf2::fromMsg(msg.pose.pose.position, init_rovio_pos);
+      init_rWI_q = rWI_q;
+      est_odom_unavail = false;
+      gt_sub.shutdown(); // stop subscribing to ground truth
+    }
+
+    tf2::Quaternion quat_diff = q_mult(sWrW_q, init_rWI_q.inverse());
+    new_msg.pose.pose.orientation = tf2::toMsg(q_mult(quat_diff,rWI_q));
     // ----------------------------------------------------------------
 
     // ROTATE rovio angular and linear velocity
     // ----------------------------------------------------------------
-    tf2::Vector3 angVel, linVel;
+    tf2::Vector3 angVel, linVel, gt_angVel, gt_linVel;
     tf2::fromMsg(msg.twist.twist.angular, angVel); // load angVel with data
     tf2::fromMsg(msg.twist.twist.linear, linVel); // load linVel with data
+    tf2::fromMsg(last_gt_msg.twist.twist.angular, gt_angVel);
+    tf2::fromMsg(last_gt_msg.twist.twist.linear, gt_linVel);
 
-    new_msg.twist.twist.angular = tf2::toMsg(quatRotate(IB_q.inverse(), angVel));
-    new_msg.twist.twist.linear  = tf2::toMsg(quatRotate(IB_q.inverse(), linVel));
+    if (do_once){
+      init_angVel = quatRotate(quat_diff.inverse(), angVel);
+      init_linVel = quatRotate(quat_diff.inverse(), linVel);
+      do_once = false;
+    }
+
+    new_msg.twist.twist.angular = tf2::toMsg(quatRotate(quat_diff.inverse(), angVel) - init_angVel + gt_angVel);
+    new_msg.twist.twist.linear  = tf2::toMsg(quatRotate(quat_diff.inverse(), linVel) - init_linVel + gt_linVel);
     // ----------------------------------------------------------------
 
     // TRANSLATE rovio to start at last groundtruth position
     // ----------------------------------------------------------------
-    geometry_msgs::Point msg_gt_p = last_gt_msg.pose.pose.position; // last ground truth position
-    new_msg.pose.pose.position = msg_gt_p; // translate to last ground truth position
+    tf2::Vector3 gt_pos;
+    tf2::fromMsg(last_gt_msg.pose.pose.position, gt_pos); // last ground truth position
+    tf2::Vector3 curr_rovio_pos;
+    tf2::fromMsg(msg.pose.pose.position, curr_rovio_pos);
+    tf2::toMsg(curr_rovio_pos - init_rovio_pos + gt_pos, new_msg.pose.pose.position); // translate to last ground truth position
     // ----------------------------------------------------------------
 
-    // Call ROVIO's reset pose service to apply the correct tranlation/rotation once
-    // ---------------------------------
-    ros::service::waitForService("/rovio/reset_to_pose", -1);
-    // reset rovio pose to the adjusted pose
-    rovio::SrvResetToPose::Request req;
-    rovio::SrvResetToPose::Response resp;
-    req.T_WM = new_msg.pose.pose;
-    if (!ros::service::call("/rovio/reset_to_pose", req, resp)){
-      ROS_ERROR("Service call failed");
-    }
-    // ---------------------------------
-
-    std::cout << "Initializing Rovio ..." << std::endl;
-    for (int i=15; i > 0; i--){
-      std::cout << "Rovio starts in " << i << " seconds" << std::endl;
-      std::this_thread::sleep_for(std::chrono::seconds(1)); // wait
-    }
-    ROS_INFO("Using Estimated Odometry");
-    est_odom_unavail = false;
-    first_rovio_callback = false;
-
-    // Shift from imu to body frame for visualization and state estimation
-    tf2::Quaternion sWB_q = q_mult(IB_q, sWI_q); // rotate sW x-axis by IB_q, then by sWI_q
-    new_msg.pose.pose.orientation = tf2::toMsg(sWB_q); // message format
-
-    //est_odom.publish(new_msg);
-    gt_sub.shutdown(); // stop subscribing to ground truth
-  }
-  else{
-    // Shift from imu to body frame for visualization and state estimation
-    tf2::Quaternion sWI_q;
-    tf2::fromMsg(msg_q, sWI_q);
-    tf2::Quaternion sWB_q = q_mult(IB_q, sWI_q); // rotate sW x-axis by IB_q, then by sWI_q
-    new_msg.pose.pose.orientation = tf2::toMsg(sWB_q); // message format
-
-    // ROTATE rovio angular and linear velocity
-    // ----------------------------------------------------------------
-    tf2::Vector3 angVel, linVel;
-    tf2::fromMsg(msg.twist.twist.angular, angVel); // load angVel with data
-    tf2::fromMsg(msg.twist.twist.linear, linVel); // load linVel with data
-    new_msg.twist.twist.angular = tf2::toMsg(quatRotate(IB_q.inverse(), angVel));
-    new_msg.twist.twist.linear  = tf2::toMsg(quatRotate(IB_q.inverse(), linVel));
-    // ----------------------------------------------------------------
-
+    // publish msg
     est_odom.publish(new_msg);
 
   }
